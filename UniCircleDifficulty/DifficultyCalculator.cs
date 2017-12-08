7ea72ac4734952b5c3ac9617d6b0c3cd9b2a8bb8
Samuel Cattini-Schultz <catts16@gmail.com>
2017-12-08 23:02:46 +1100

    Restructured DifficultyCalculator

--- UniCircleDifficulty/DifficultyCalculator.cs
+++ UniCircleDifficulty/DifficultyCalculator.cs
@@ -9,31 +9,51 @@ namespace UniCircleDifficulty
     {
         private Beatmap _beatmap;
         private Mods _mods;
-        
+        private bool _calculated;
+
+        private Aim _aim;
+        private Speed _speed;
+        private Accuracy _accuracy;
+        private Reading _reading;
+
+        public double AimDifficulty { get => _aim.Value; }
+        public double SpeedDifficulty { get => _speed.Value; }
+        public double AccuracyDifficulty { get => _accuracy.Value; }
+        public double ReadingDifficulty { get => _reading.Value; }
+
+        public double Difficulty {
+            get
+            {
+                if (!_calculated)
+                {
+                    CalculateDifficulty();
+                }
+                return AimDifficulty + SpeedDifficulty + AccuracyDifficulty + ReadingDifficulty;
+            }
+        }
+
         public DifficultyCalculator(Beatmap beatmap, Mods mods = Mods.None)
         {
             _beatmap = beatmap;
             _mods = mods;
+            _calculated = false;
             PreprocessBeatmap();
         }
 
-        public double CalculateDifficulty()
+        public void CalculateDifficulty()
         {
             // Calculates skill difficulties
-            Aim aim = new Aim();
-            Speed speed = new Speed();
-            Accuracy accuracy = new Accuracy();
-            Reading reading = new Reading();
+            _aim = new Aim();
+            _speed = new Speed();
+            _accuracy = new Accuracy();
+            _reading = new Reading();
 
-            aim.ProcessHitObjectSequence(_beatmap.HitObjects);
-            speed.ProcessHitObjectSequence(_beatmap.HitObjects);
+            _aim.ProcessHitObjectSequence(_beatmap.HitObjects);
+            _speed.ProcessHitObjectSequence(_beatmap.HitObjects);
+            _accuracy.ProcessHitObjectSequence(_beatmap.HitObjects);
+            _reading.ProcessHitObjectSequence(_beatmap.HitObjects);
 
-            Console.WriteLine("AIM: " + aim.Value);
-            Console.WriteLine("SPEED: " + speed.Value);
-            Console.WriteLine("ACCURACY: " + accuracy.Value);
-            Console.WriteLine("READING: " + reading.Value);
-
-            return aim.Value + speed.Value + accuracy.Value + reading.Value;
+            _calculated = true;
         }
 
         private void PreprocessBeatmap()
